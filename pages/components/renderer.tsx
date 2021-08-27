9034ccdf1bae8aa9834910d787b4a2d98dfd2fe8
Lee Danny <lhlxtl@gmail.com>
2021-08-27 11:17:08 +0800

    梳理依赖

--- pages/components/renderer.tsx
+++ pages/components/renderer.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { NodeViewWrapper, NodeViewContent } from '@tiptap/react'
 import { MODE } from './extension';
+import dynamic from 'next/dynamic';
+const Graphviz = dynamic(() => import('graphviz-react'), { ssr: false });
 
 export default (props) => {
-  console.log(props)
   const mode = props.node.attrs.mode;
   const content = props.node.textContent;
   const previewer = React.useRef(null);
@@ -13,21 +14,13 @@ export default (props) => {
       mode: newMode
     });
   }, [props, mode]);
-  React.useEffect(() => {
-    console.log(previewer);
-    if (!previewer.current) return; 
-    try {
-      d3.select('#preview').graphviz().renderDot(content);
-    } catch (e) {
-      // TODO: 处理异常
-      console.log(e);
-    }
-  }, [previewer, mode, content]);
   return (
     <NodeViewWrapper className="react-component-with-content" contentEditable={mode === MODE.EDIT}>
       <span className="label" onClick={toggleMode} contentEditable={false}>{mode === MODE.PREVIEW ? '编辑' : '预览'}</span>
       <div hidden={mode === MODE.PREVIEW}><NodeViewContent className="content" /></div>
-      <div hidden={mode === MODE.EDIT} id="preview" className="content" ref={previewer}></div>
+      <div hidden={mode === MODE.EDIT} id="preview" className="content" ref={previewer}>
+        <Graphviz dot={content}/>
+      </div>
     </NodeViewWrapper>
   )
 }
